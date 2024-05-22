c22ce6d7dc5de3b63798a261adccf758687cafbe
Antonio00827 <23054@student.macleans.school.nz>
2024-05-22 16:33:24 +1200

    Create Beginners4.py

---
+++ Beginners4.py
@@ -0,0 +1,10 @@
+a = int(input("Integer"))
+b = input("Name")
+print(a*b)
+c = int(input("Positive integer"))
+if c < 0:
+  print("Not a positive number")
+else:
+  for i in range (12):
+    print(i*c)
+d = int(input())
